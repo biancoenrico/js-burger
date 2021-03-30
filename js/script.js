66d0896d0eef53c34de33dd95a2305b816a86d60
enrico bianco <biancoenrico71@gmail.com>
2021-03-30 17:34:59 +0200

    finita logica pagina

--- js/script.js
+++ js/script.js
@@ -5,15 +5,20 @@ var costoBase = 50
 var calculate = document.getElementById('button')
 calculate.addEventListener('click', function(){
 
+    var nomePanino = document.getElementById('mio-nome').value
     var ingredient = document.getElementsByClassName('addon')
     console.log(ingredient);
+
+    if(nomePanino == ''){
+        alert('dai un nome al tuo panino')
+    }
 
     for (var i = 0; i < ingredient.length; i++) {
         if (ingredient[i].checked) {
             costoBase = costoBase + parseInt(ingredient[i].value);
         }
-        
     }
+
     var coupon = document.getElementById('coupon').value;
     if(coupon == 'sconto123' || coupon == 'tantafantasia' || coupon == 'fammimangiare' || coupon == 'sotirchio'){
        costoBase = costoBase - costoBase * 0.20;
@@ -21,5 +26,10 @@ calculate.addEventListener('click', function(){
 
     document.getElementById('tot').innerHTML ='$' + costoBase;
 
+    if(nomePanino == ''){
+        document.getElementById("button").disabled = false; 
+    }else{
+        document.getElementById("button").disabled = true; 
+    }
 })
 
